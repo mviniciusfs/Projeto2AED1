62760e80488a4ecd449a3e28ab875b861ae74b9b
mviniciusfs <50427044+mviniciusfs@users.noreply.github.com>
2019-12-07 11:32:19 -0300

    Nova versão cm Lançamento de Exceções
    Throw New;
    
    Corrigir Try Catch de idade...

--- main.cs
+++ main.cs
@@ -6,180 +6,225 @@ class MainClass
 {
   public static void Main (string[] args) 
   {
-    Console.WriteLine("Cadastro Pessoa baixa renda");
+    //Título - Utilizar PROMPT MAXIMIZADO!!!
+    StreamReader z;
+
+    string CaminhoT = "Titulo.txt";
+
+    z = File.OpenText(CaminhoT);
+
+    while(z.EndOfStream != true)
+    {
+      string linha = z.ReadLine();
+      Console.WriteLine(linha);
+    }
+
+    z.Close();
+    Console.WriteLine();
+    Console.WriteLine("           **Cadastro de Pessoas de baixa renda**");
     Console.WriteLine();
 
     Console.Write("Deseja cadastrar uma nova Pessoa? ");
     string cad = Console.ReadLine().ToUpper();
 
-    //Entrada dos dados pelo usuário
-    while(cad == "SIM" || cad == "S")
+    if (cad == "N" || cad == "NAO")
     {
-      Console.Write("Digite o Nome: ");
-      string nome = Console.ReadLine();
+      Console.WriteLine();
+      Console.Write("PROGRAMA FINALIZADO");
+      Environment.Exit(0);           
+    }
 
-      Console.Write("Digite o Cargo: ");
-      string cargo = Console.ReadLine();
+    if(cad == "S" || cad == "NAO")
+    {
 
-      //Tratamento de exceções
-      bool invalido = true;
-      int idade = 0;
-      double salario = 0;
-
-      do
+      //Entrada dos dados pelo usuário
+      while(cad == "SIM" || cad == "S")
       {
-        try
-        {
-          Console.Write("Digite a Idade: ");
-          idade = int.Parse(Console.ReadLine());
-          invalido = false;
-        }
-        catch (FormatException)
-        {
-          Console.WriteLine("Dados inválidos favor inserir novamente!");
-          invalido = true;
-        }
-      }
-      while (invalido);
-            
-      Console.Write("Digite a Quantidade de Filhos: ");
-      int qtdFilhos = int.Parse(Console.ReadLine());
+        Console.Write("Digite o Nome: ");
+        string nome = Console.ReadLine();
 
-      do
-      {
-        try
+        Console.Write("Digite o Cargo: ");
+        string cargo = Console.ReadLine();
+
+        //Tratamento de exceções
+        bool invalido = true;
+        int idade = 0;
+        double salario = 0;
+
+        do
         {
-          Console.Write("Digite o Salário: ");
-          salario = double.Parse(Console.ReadLine());
-          invalido = false;
-        }
-        catch (FormatException)
-        {
-          Console.WriteLine("Dados inválidos favor inserir novamente!");
-          invalido = true;
-        }
-      }
-      while (invalido);
-      
-
-      //Validação para BENEFICIÁRIOS;
-      if(salario <= 1100)
-      {        
-        //Trabalhando com arquivo .txt
-        StreamWriter x;
-        string CaminhoNome = "dados.txt";
-        x = File.AppendText(CaminhoNome);
-
-        //Salvando dados no arquivo;
-        Administrador.AddResponsavel(new Responsavel(nome, idade, cargo, salario, qtdFilhos));
-        List <Responsavel>Responsaveis = Administrador.getListaResponsavel();      
-        foreach(Responsavel responsavel in  Responsaveis)
-        {
-          x.WriteLine(responsavel.Imprimir().ToUpper());       
-        }
-
-        x.Close();
-
-        if(qtdFilhos > 0)
-        {
-          //Cadastro de Filho - Herança
-          Console.Write("Deseja realizar o cadastro de filho(s) do {0}? ", nome);
-          string cad4 = Console.ReadLine().ToUpper();
-
-          //Leitura dos dados;
-          while(cad4 == "SIM" || cad4 == "S")
+          try
           {
-            Console.Write("Digite o Nome: ");
-            string nome1 = Console.ReadLine();
-
-            Console.Write("Nome da Escola: : ");
-            string nome_sch = Console.ReadLine();
-
-            Console.Write("Digite a Série: ");
-            string serie_sch = Console.ReadLine();
-
-            Console.Write("Possui alguma deficiência: ");
-            string def_fis = Console.ReadLine();
-
-            //Tratamento de exceções
-            bool invalido1 = true;
-            int idade1 = 0;
-            
-            do
-            {
-              try
-              {
-                Console.Write("Digite a Idade: ");
-                idade = int.Parse(Console.ReadLine());
-                invalido = false;
-              }
-              catch (FormatException)
-              {
-                Console.WriteLine("Idade inválida, favor inserir novamente!");
-                invalido = true;
-              }
-            }
-            while (invalido);       
-                
-            //Salvando dados do filho
-            //Trabalhando com arquivo .txt
-            StreamWriter x1;
-            string CaminhoNome1 = "dadosfilhos.txt";
-            x = File.AppendText(CaminhoNome);
-
-            //Salvando dados no arquivo;
-            Administrador.AddFilho(new Filho(nome1, idade, nome_sch, serie_sch, def_fis));
-            List <Filho>Filhos = Administrador.getListaFilho();      
-            foreach(Filho filho in  Filhos)
-            {
-              x.WriteLine(filho.Imprimir().ToUpper());       
-            }
-
-            x.Close();
-
-            if(qtdFilhos >= 2)
-            {
-              Console.Write("Possui mais algum filho?  ");
-              string cad5 = Console.ReadLine().ToUpper();
+            Console.Write("Digite a Idade: ");
+            idade = int.Parse(Console.ReadLine());
+            invalido = false;
+          }
+          catch (FormatException)
+          {
+            Console.WriteLine("Dados inválidos favor inserir novamente!");
+            invalido = true;
+          }
+        }
+        while (invalido);
               
-              if(cad5 == "NAO" || cad5 == "N")
-              {
-                break;
-              } 
+        Console.Write("Digite a Quantidade de Filhos: ");
+        int qtdFilhos = int.Parse(Console.ReadLine());
 
-            }
+        do
+        {
+          try
+          {
+            Console.Write("Digite o Salário: ");
+            salario = double.Parse(Console.ReadLine());
+            invalido = false;
+          }
+          catch (FormatException)
+          {
+            Console.WriteLine("Dados inválidos favor inserir novamente!");
+            invalido = true;
+          }
+        }
+        while (invalido);
+        
 
+        //Validação para BENEFICIÁRIOS;
+        if(salario <= 1100)
+        {        
+          //Trabalhando com arquivo .txt
+          StreamWriter x;
+          string CaminhoNome = "dados.txt";
+          x = File.AppendText(CaminhoNome);
+
+          //Salvando dados no arquivo;
+          Administrador.AddResponsavel(new Responsavel(nome, idade, cargo, salario, qtdFilhos));
+          List <Responsavel>Responsaveis = Administrador.getListaResponsavel();      
+          foreach(Responsavel responsavel in  Responsaveis)
+          {
+            x.WriteLine(responsavel.Imprimir().ToUpper());       
           }
 
-        }  
+          x.Close();
+
+          if(qtdFilhos > 0)
+          {
+            //Cadastro de Filho - Herança
+            Console.Write("Deseja realizar o cadastro de filho(s) do {0}? ", nome);
+            string cad4 = Console.ReadLine().ToUpper();
+
+            //Leitura dos dados;
+            while(cad4 == "SIM" || cad4 == "S")
+            {
+              Console.Write("Digite o Nome: ");
+              string nome1 = Console.ReadLine();
+
+              Console.Write("Nome da Escola: : ");
+              string nome_sch = Console.ReadLine();
+
+              Console.Write("Digite a Série: ");
+              string serie_sch = Console.ReadLine();
+
+              Console.Write("Possui alguma deficiência: ");
+              string def_fis = Console.ReadLine();
+
+              //Tratamento de exceções
+              //bool invalido1 = true;
+              //int idade1 = 0;
+              
+              do
+              {
+                try
+                {
+                  Console.Write("Digite a Idade: ");
+                  idade = int.Parse(Console.ReadLine());
+                  invalido = false;
+                }
+                catch (FormatException)
+                {
+                  Console.WriteLine("Idade inválida, favor inserir novamente!");
+                  invalido = true;
+                }
+              }
+              while (invalido);       
+                  
+              //Salvando dados do filho
+              //Trabalhando com arquivo .txt
+              //StreamWriter x1;
+              //string CaminhoNome1 = "dadosfilhos.txt";
+              //x = File.AppendText(CaminhoNome);
+
+              //Salvando dados no arquivo;
+              Administrador.AddFilho(new Filho(nome1, idade, nome_sch, serie_sch, def_fis));
+              List <Filho>Filhos = Administrador.getListaFilho();      
+              foreach(Filho filho in  Filhos)
+              {
+                x.WriteLine(filho.Imprimir().ToUpper());       
+              }
+
+              x.Close();
+
+              if(qtdFilhos >= 2)
+              {
+                Console.Write("Deseja cadastrar o outro filho(a)?  ");
+                string cad5 = Console.ReadLine().ToUpper();
+                
+                if(cad5 == "NAO" || cad5 == "N")
+                {
+                  break;
+                }
+
+                 
+
+              }
+
+            }
+
+          }  
 
 
-        Console.Write("Deseja cadastrar uma nova Pessoa? ");
-        string cad2 = Console.ReadLine().ToUpper();
+          Console.Write("Deseja cadastrar uma nova Pessoa? ");
+          string cad2 = Console.ReadLine().ToUpper();
 
-        if(cad2 == "NAO" || cad2 == "N")
-        {
-          break;
+          
+          if(cad2 == "NAO" || cad2 == "N")
+          {
+            break;
+          }
+			//Usando tratamento de esceções
+          else if(cad2 != "SIM" && cad2!= "S")
+          {
+            throw new System.ArgumentException("INVÁLIDO: Programa finalizado");
+          }
+          
         }
-        
+
+        else if(salario > 1100)
+        {
+          Console.WriteLine("**********ATENÇÃO*********");
+          Console.WriteLine("APENAS CADASTROS DE BAIXA RENDA - SALÁRIO ATÉ R$1100,00");
+
+          Console.WriteLine();
+
+          Console.Write("Deseja cadastrar uma nova Pessoa? ");
+          string cad3 = Console.ReadLine().ToUpper();
+
+          if(cad3 == "NAO" || cad3 == "N")
+          {
+            break;
+          }
+        }
       }
 
-      else if(salario > 1100)
-      {
-        Console.WriteLine("**********ATENÇÃO*********");
-        Console.WriteLine("APENAS CADASTROS DE BAIXA RENDA - SALÁRIO ATÉ R$1100,00");
-
-        Console.WriteLine();
-
-        Console.Write("Deseja cadastrar uma nova Pessoa? ");
-        string cad3 = Console.ReadLine().ToUpper();
-
-        if(cad3 == "NAO" || cad3 == "N")
-        {
-          break;
-        }
-      }
     }
+	//usando tratamento de esceções
+    else
+    {
+      throw new Exception("AVISO: OPÇÃO INVALIDA. PROGRAMA FINALIZADO!");
+
+    }
+         
+  
+
+    
 
 
     
@@ -216,6 +261,17 @@ class MainClass
       Console.WriteLine();
       Console.Write("********PROGRAMA FINALIZADO*********");
      
+    }
+
+    if(resultfinal == "NAO" || resultfinal == "N")
+    {
+      Console.Write("********PROGRAMA FINALIZADO*********");
+
+    }
+	//usando tratamento de exceções
+    else
+    {
+      throw new System.ArgumentException("INVÁLIDO: Programa finalizado");
     }
     
   }
